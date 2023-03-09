6a640f948fdab5cf90a304563e6182fd09898e40
USER <USER@USER-BILGISAYAR>
2023-03-09 14:28:13 +0300

    Add project files.

---
+++ ConsoleApp1/Musteri.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    internal class Musteri
+    {
+        public string Ad; //gerçek olan String, string: alias
+        public string Soyad;
+        public int MusteriNo;
+        public decimal Bakiye;
+        public decimal IndirimYap(decimal indirim)
+        {
+            return Bakiye - indirim;
+
+        }
+
+        public override string ToString()
+        {
+            return Ad + " " + Soyad;
+        }
+
+    }
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿//Console.WriteLine("Hello, World!");
+
+
+class Program1
+{
+    public static void Main()
+    {
+        Musteri m1=new Musteri();
+        m1.MusteriNo = 1;
+        m1.Ad = "Hasan";
+        m1.Soyad = "Demir";
+        m1.Bakiye = 100;
+
+        Musteri m2 = new Musteri();
+        m2.MusteriNo = 1;
+        m2.Ad = "Ayşe";
+        m2.Soyad = "Fidan";
+        m2.Bakiye = 200;
+
+        Console.WriteLine(m1);
+        Console.WriteLine(m1.IndirimYap(50));
+        Console.WriteLine(m2);
+        Console.WriteLine(m2.IndirimYap(60));
+
+        int j = 5;//Int16  veya Int32 veya Int64 i=5;
+        if (j == 5)
+        {
+            Console.WriteLine("Eşit");
+
+        }
+        else {
+            Console.WriteLine("Eşit Değil");
+        }
+        for (int i = 0; i < j; i++) //i=i+2
+        {
+            Console.WriteLine(i);
+        }
+
+    } //End Main
+}
+
+
